4e28ee05cdf5250210f7c80f0997ce2866a4c1c0
Adelina_Santos <adevitoria.m09@gmail.com>
2025-02-25 15:03:20 -0300

    Aplicação de pages Sobre

---
+++ src/pages/sobre/Sobre.tsx
@@ -0,0 +1,51 @@
+export default function Sobre() {
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <div> 
+                <h1 className="text-4xl font-bold pb-10">Sobre Nós </h1>
+            </div>
+            <div>
+                <p>
+                    Adelina Santos - Desenvolvedora
+                    <br />
+                    Github: <a href="https://github.com/Adelina2801" target="_blank" rel="noopener noreferrer">github.com/Adelina2801</a>
+                </p>
+                <p>
+                    <br />
+                    <br />
+                    Beatriz Borges - Tester
+                    <br />
+                    Github: <a href="https://github.com/beasb" target="_blank" rel="noopener noreferrer">github.com/beasb</a>
+                </p>
+                <p>
+                    <br />
+                    <br />
+                    Denner dos Anjos - Desenvolvedor
+                    <br />
+                    Github: <a href="https://github.com/DennerASilva" target="_blank" rel="noopener noreferrer">github.com/DennerASilva</a>
+                </p>
+                <p>
+                    <br />
+                    <br />
+                    Lucas Pimentel - Scrum Master
+                    <br />
+                    Github: <a href="https://github.com/Pimentelucas" target="_blank" rel="noopener noreferrer">github.com/Pimentelucas</a>
+                </p>
+                <p>
+                    <br />
+                    <br />
+                    Maria Eduarda - PO
+                    <br />
+                    Github: <a href="https://github.com/mariacosta2203" target="_blank" rel="noopener noreferrer">github.com/mariacosta2203</a>
+                </p>
+                <p>
+                    <br />
+                    <br />
+                    Otavio Ferreira - Desenvolvedor
+                    <br />
+                    Github: <a href="https://github.com/CURINGU" target="_blank" rel="noopener noreferrer">github.com/CURINGU</a>
+                </p>
+            </div>
+        </div>
+    );
+}
